aaf972967f4f06b2165607852882cbec695d3860
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-06-11 13:06:10 +0200

    Entity inspector
    With the entity inspector we have parsed all related joins and entities
    available in memory for quick look up.

---
+++ src/EntityInspector.php
@@ -0,0 +1,89 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\React\SimpleORM;
+
+use Doctrine\Common\Annotations\Reader;
+use ReflectionClass;
+use ReflectionProperty;
+use Roave\BetterReflection\BetterReflection;
+use function WyriHaximus\iteratorOrArrayToArray;
+use WyriHaximus\React\SimpleORM\Annotation\JoinInterface;
+use WyriHaximus\React\SimpleORM\Annotation\Table;
+use WyriHaximus\React\SimpleORM\Entity\Field;
+use WyriHaximus\React\SimpleORM\Entity\Join;
+
+final class EntityInspector
+{
+    /** @var Reader */
+    private $annotationReader;
+
+    /** @var InspectedEntity[] */
+    private $entities = [];
+
+    public function __construct(Reader $annotationReader)
+    {
+        $this->annotationReader = $annotationReader;
+    }
+
+    public function getEntity(string $entity): InspectedEntity
+    {
+        if (!isset($this->entities[$entity])) {
+            $class = new ReflectionClass($entity);
+            $tableAnnotation = $this->annotationReader->getClassAnnotation($class, Table::class);
+
+            if ($tableAnnotation instanceof Table === false) {
+                throw new \RuntimeException('Missing Table annotation on entity: ' . $entity);
+            }
+
+            $joins = iteratorOrArrayToArray($this->getJoins($class));
+            $this->entities[$entity] = new InspectedEntity(
+                $entity,
+                $tableAnnotation->getTable(),
+                iteratorOrArrayToArray($this->getFields($class, $joins)),
+                $joins
+            );
+        }
+
+        return $this->entities[$entity];
+    }
+
+    private function getFields(ReflectionClass $class, array $joins): iterable
+    {
+        /** @var ReflectionProperty $property */
+        foreach ($class->getProperties() as $property) {
+            if (isset($joins[$property->getName()])) {
+                continue;
+            }
+
+            /** @psalm-suppress PossiblyNullReference */
+            yield $property->getName() => new Field(
+                $property->getName(),
+                (string)\current((new BetterReflection())
+                    ->classReflector()
+                    ->reflect($class->getName())->getProperty($property->getName())->getDocBlockTypes())
+            );
+        }
+    }
+
+    private function getJoins(ReflectionClass $class): iterable
+    {
+        $annotations = $this->annotationReader->getClassAnnotations($class);
+
+        /** @var object|JoinInterface $annotation */
+        foreach ($annotations as $annotation) {
+            if ($annotation instanceof JoinInterface === false) {
+                continue;
+            }
+
+            yield $annotation->getProperty() => new Join(
+                $this->getEntity($annotation->getEntity()),
+                $annotation->getType(),
+                $annotation->getLocalKey(),
+                $annotation->getLocalCast(),
+                $annotation->getForeignKey(),
+                $annotation->getForeignCast(),
+                $annotation->getProperty()
+            );
+        }
+    }
+}
---
+++ tests/EntityInspectorTest.php
@@ -0,0 +1,106 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\React\Tests\SimpleORM;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use WyriHaximus\AsyncTestUtilities\AsyncTestCase;
+use WyriHaximus\React\SimpleORM\EntityInspector;
+use WyriHaximus\React\Tests\SimpleORM\Stub\BlogPostStub;
+use WyriHaximus\React\Tests\SimpleORM\Stub\CommentStub;
+use WyriHaximus\React\Tests\SimpleORM\Stub\NoSQLStub;
+use WyriHaximus\React\Tests\SimpleORM\Stub\UserStub;
+
+/**
+ * @internal
+ */
+final class EntityInspectorTest extends AsyncTestCase
+{
+    /** @var EntityInspector */
+    private $entityInspector;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->entityInspector = new EntityInspector(new AnnotationReader());
+    }
+
+    /**
+     * @test
+     */
+    public function inspect(): void
+    {
+        $inspectedEntity = $this->entityInspector->getEntity(UserStub::class);
+
+        self::assertSame(UserStub::class, $inspectedEntity->getClass());
+        self::assertSame('users', $inspectedEntity->getTable());
+
+        $fields = $inspectedEntity->getFields();
+        self::assertCount(2, $fields);
+        self::assertArrayHasKey('id', $fields);
+        self::assertSame('int', $fields['id']->getType());
+        self::assertArrayHasKey('name', $fields);
+        self::assertSame('string', $fields['name']->getType());
+    }
+
+    /**
+     * @test
+     */
+    public function inspectWithJoins(): void
+    {
+        $inspectedEntity = $this->entityInspector->getEntity(BlogPostStub::class);
+
+        self::assertSame(BlogPostStub::class, $inspectedEntity->getClass());
+        self::assertSame('blog_posts', $inspectedEntity->getTable());
+
+        $fields = $inspectedEntity->getFields();
+        self::assertCount(5, $fields);
+
+        foreach ([
+            'id' => 'int',
+            'author_id' => 'int',
+            'title' => 'string',
+            'contents' => 'string',
+        ] as $key => $type) {
+            self::assertArrayHasKey($key, $fields, $key);
+            self::assertSame($type, $fields[$key]->getType(), $key);
+        }
+
+        $joins = $inspectedEntity->getJoins();
+        self::assertCount(3, $joins);
+
+        self::assertArrayHasKey('author', $joins);
+        self::assertSame(UserStub::class, $joins['author']->getEntity()->getClass());
+        self::assertSame('author_id', $joins['author']->getLocalKey());
+        self::assertNull($joins['author']->getLocalCast());
+        self::assertSame('id', $joins['author']->getForeignKey());
+        self::assertNull($joins['author']->getForeignCast());
+        self::assertSame('author', $joins['author']->getProperty());
+
+        self::assertSame(CommentStub::class, $joins['comments']->getEntity()->getClass());
+        self::assertSame('id', $joins['comments']->getLocalKey());
+        self::assertSame('BIGINT', $joins['comments']->getLocalCast());
+        self::assertSame('blog_post_id', $joins['comments']->getForeignKey());
+        self::assertNull($joins['comments']->getForeignCast());
+        self::assertSame('comments', $joins['comments']->getProperty());
+
+        self::assertArrayHasKey('author', $joins['comments']->getEntity()->getJoins());
+        self::assertSame(UserStub::class, $joins['comments']->getEntity()->getJoins()['author']->getEntity()->getClass());
+        self::assertSame('author_id', $joins['comments']->getEntity()->getJoins()['author']->getLocalKey());
+        self::assertNull($joins['comments']->getEntity()->getJoins()['author']->getLocalCast());
+        self::assertSame('id', $joins['comments']->getEntity()->getJoins()['author']->getForeignKey());
+        self::assertNull($joins['comments']->getEntity()->getJoins()['author']->getForeignCast());
+        self::assertSame('author', $joins['comments']->getEntity()->getJoins()['author']->getProperty());
+    }
+
+    /**
+     * @test
+     */
+    public function inspectWithoutTable(): void
+    {
+        self::expectException(\RuntimeException::class);
+        self::expectExceptionMessage('Missing Table annotation on entity: ' . NoSQLStub::class);
+
+        $this->entityInspector->getEntity(NoSQLStub::class);
+    }
+}
