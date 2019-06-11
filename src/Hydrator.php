6271d066fedfb3354384c192609c3f60e0400852
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-06-11 13:12:43 +0200

    Hydrate using the new tree structure

--- src/Hydrator.php
+++ src/Hydrator.php
@@ -3,24 +3,63 @@
 namespace WyriHaximus\React\SimpleORM;
 
 use GeneratedHydrator\Configuration;
+use WyriHaximus\React\SimpleORM\Entity\Field;
 
 final class Hydrator
 {
-    /**
-     * @var string[]
-     */
+    /** @var string[] */
     private $hydrators = [];
 
-    public function hydrate(string $class, array $data): object
+    public function hydrate(InspectedEntity $inspectedEntity, array $data): EntityInterface
     {
+        $class = $inspectedEntity->getClass();
         if (!isset($this->hydrators[$class])) {
-            $this->hydrators[$class] = (function ($class) {
+            /**
+             * @psalm-suppress MissingClosureReturnType
+             * @psalm-suppress InvalidPropertyAssignmentValue
+             */
+            $this->hydrators[$inspectedEntity->getClass()] = (function (string $class) {
                 $hydratorClass = (new Configuration($class))->createFactory()->getHydratorClass();
 
+                /** @psalm-suppress InvalidStringClass */
                 return new $hydratorClass();
             })($class);
         }
 
+        foreach ($data as $key => $value) {
+            if (isset($inspectedEntity->getFields()[$key])) {
+                $data[$key] = $this->castValueToCorrectType($inspectedEntity->getFields()[$key], $value);
+            }
+        }
+
+        foreach ($inspectedEntity->getJoins() as $join) {
+            if (\is_array($data[$join->getProperty()])) {
+                $data[$join->getProperty()] = $this->hydrate(
+                    $join->getEntity(),
+                    $data[$join->getProperty()]
+                );
+            }
+        }
+
+        /**
+         * @psalm-suppress PossiblyNullReference
+         * @psalm-suppress InvalidMethodCall
+         */
         return $this->hydrators[$class]->hydrate($data, new $class());
+    }
+
+    /**
+     * @param Field $field
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    private function castValueToCorrectType(Field $field, $value)
+    {
+        if ($field->getType() === 'int') {
+            return (int) $value;
+        }
+
+        return $value;
     }
 }
--- tests/HydratorTest.php
+++ tests/HydratorTest.php
@@ -2,7 +2,12 @@
 
 namespace WyriHaximus\React\Tests\SimpleORM;
 
+use function ApiClients\Tools\Rx\observableFromArray;
+use Doctrine\Common\Annotations\AnnotationReader;
+use WyriHaximus\React\SimpleORM\EntityInspector;
 use WyriHaximus\React\SimpleORM\Hydrator;
+use WyriHaximus\React\Tests\SimpleORM\Stub\BlogPostStub;
+use WyriHaximus\React\Tests\SimpleORM\Stub\UserStub;
 use WyriHaximus\TestUtilities\TestCase;
 
 /**
@@ -13,15 +18,53 @@ final class HydratorTest extends TestCase
     public function testHydrate(): void
     {
         $id = 123;
-        $title = 'null';
+        $title = 'tables.title';
 
-        /** @var EntityStub $entity */
-        $entity = (new Hydrator())->hydrate(EntityStub::class, [
-            'id' => $id,
-            'title' => $title,
-        ]);
+        /** @var UserStub $entity */
+        $entity = (new Hydrator())->hydrate(
+            (new EntityInspector(new AnnotationReader()))->getEntity(UserStub::class),
+            [
+                'id' => $id,
+                'name' => $title,
+            ]
+        );
+
+        self::assertSame($id, $entity->getId());
+        self::assertSame($title, $entity->getName());
+    }
+
+    public function testHydrateWithJoins(): void
+    {
+        $id = 123;
+        $title = 'null';
+        $authorId = 1;
+        $authorName = 'llun';
+        $publisherId = 2;
+        $publisherName = 'dasdsadas';
+
+        /** @var BlogPostStub $entity */
+        $entity = (new Hydrator())->hydrate(
+            (new EntityInspector(new AnnotationReader()))->getEntity(BlogPostStub::class),
+            [
+                'id' => $id,
+                'title' => $title,
+                'author' => [
+                    'id' => $authorId,
+                    'name' => $authorName,
+                ],
+                'publisher' => [
+                    'id' => $publisherId,
+                    'name' => $publisherName,
+                ],
+                'comments' => observableFromArray([]),
+            ]
+        );
 
         self::assertSame($id, $entity->getId());
         self::assertSame($title, $entity->getTitle());
+        self::assertSame($authorId, $entity->getAuthor()->getId());
+        self::assertSame($authorName, $entity->getAuthor()->getName());
+        self::assertSame($publisherId, $entity->getPublisher()->getId());
+        self::assertSame($publisherName, $entity->getPublisher()->getName());
     }
 }
