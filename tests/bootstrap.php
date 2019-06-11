f90757ef21c57f2776e97454081dce375695a8f8
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-06-11 12:45:33 +0200

    .env testsuite config loader

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+use Dotenv\Dotenv;
+
+require \dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
+
+Dotenv::create(\dirname(__DIR__))->load();
